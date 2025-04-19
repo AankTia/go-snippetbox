56a1ef674f40a3c9132af9d1bbcdb77d499e4a74
TiaWidi <tia.septianawidi@gmail.com>
2025-04-19 20:49:57 +0700

    Routing requests

--- main.go
+++ main.go
@@ -11,11 +11,21 @@ func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello from Snippetbox"))
 }
 
+func snippetView(w http.ResponseWriter, r *http.Request)  {
+	w.Write([]byte("Display a specific snippet..."))
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Create a new snippet..."))
+}
+
 func main() {
 	// Use the http.NewServerMux() funtion to initialize a new servermux, then
 	// register the home function as the handler of the "/" URL pattern.
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
 
 	// Use the http.ListenAndServe() function to start a new web server.
 	// We pass i two parameters: the TCP network address to listen on (in this case ":400")
