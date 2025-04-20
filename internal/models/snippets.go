145b4acab8d4b2fe1b51af4e3492251d201e5986
TiaWidi <tia.septianawidi@gmail.com>
2025-04-21 01:00:31 +0700

    Designing a database model

---
+++ internal/models/snippets.go
@@ -0,0 +1,34 @@
+package models
+
+import (
+	"database/sql"
+	"time"
+)
+
+type Snippet struct {
+	ID      int
+	Title   string
+	Content string
+	Created time.Time
+	Expires time.Time
+}
+
+// Define a SnippetModel type which wraps a sql.DB connectin pool.
+type SnippetModel struct {
+	DB *sql.DB
+}
+
+// Insert a new snippet into the database.
+func (m *SnippetModel) Insert(title string, content string, expires int) (int, error) {
+	return 0, nil
+}
+
+// Return a specific snippet base on its id.
+func (m *SnippetModel) Get(id int) (*Snippet, error)  {
+	return nil, nil
+}
+
+// Return the 10 most recently created snippets.
+func (m *SnippetModel) Latest() ([]*Snippet, error)  {
+	return nil, nil	
+}
