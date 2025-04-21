49523baf0ef51717076309db4c2ab1d5ee1c0265
TiaWidi <tia.septianawidi@gmail.com>
2025-04-21 07:50:04 +0700

    Executing the query

--- internal/models/snippets.go
+++ internal/models/snippets.go
@@ -20,15 +20,35 @@ type SnippetModel struct {
 
 // Insert a new snippet into the database.
 func (m *SnippetModel) Insert(title string, content string, expires int) (int, error) {
-	return 0, nil
+	stmt := `
+		INSERT INTO snippets (title, content, created, expires)
+		VALUES(?, ?, UTC_TIMESTAMP(), DATE_ADD(UTC_TIMESTAMP(), INTERVAL ? DAY))
+	`
+
+	// Use the Exec() method on the embedded connection pool to execute the statement.
+	// This method returns a sql.Result type, which contains some basic
+	// information about happened when the statement was executed.
+	result, err := m.DB.Exec(stmt, title, content, expires)
+	if err != nil {
+		return 0, err
+	}
+
+	// Use the LastInsertId() method on the result toget the ID of newly inserted in the snippets table.
+	id, err := result.LastInsertId()
+	if err != nil {
+		return 0, err
+	}
+
+	// The ID returned has type int64, so we convert it on an int type before returning
+	return int(id), nil
 }
 
 // Return a specific snippet base on its id.
-func (m *SnippetModel) Get(id int) (*Snippet, error)  {
+func (m *SnippetModel) Get(id int) (*Snippet, error) {
 	return nil, nil
 }
 
 // Return the 10 most recently created snippets.
-func (m *SnippetModel) Latest() ([]*Snippet, error)  {
-	return nil, nil	
+func (m *SnippetModel) Latest() ([]*Snippet, error) {
+	return nil, nil
 }
